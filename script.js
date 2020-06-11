23e436c5e792e89b3fd0b8fd3d3d5e00291851ab
byunw <byunw@bc.edu>
2020-06-10 22:30:28 -0400

    wed

--- script.js
+++ script.js
@@ -1,35 +1,75 @@
 
-//document.getElementById("combination").innerHTML="alex1425";
 
-//10 combinations to show
-//var combination=["qwer90vd","ddfnvkei123","dfvinwe92","dnkfdnfkd","89df0212","dfdwe335","opwes22","hohodcw26","gjtunb9f","aleeex"];
-
-
-//test case
-var combination=["alex","byun","david","daniel"]
+var combination=["qwer90vd","ddfnvkei123","dfvinwe92","dnkfdnfkd","89df0212","dfdwe335","opwes22","hohodcw26","gjtunb9f","aleeex"];
+var score=0;
 
 
 function show_disappear(i){
 
-    if(i<0)return;
-    
+    if(i<0){
+
+        document.getElementById("user-score").innerHTML="Final Score: "+score.toString();
+        document.getElementById("button").remove();
+        return;
+
+    }
+
     document.getElementById("combination").innerHTML=combination[i];
     
-    //wait for 3 seconds and make the combination disappear
     setTimeout(function(){
-        document.getElementById("combination").innerHTML="";    
+        
+        document.getElementById("combination").innerHTML="";
+        document.getElementById("instruction").innerHTML="You got 10 seconds to type what you memorized in the following answer box:";
+        var input=document.createElement("input");
+        input.type="text";
+        input.id="answer";
+        document.getElementById("form").appendChild(input);
+
+
     },3000);
 
-    //wait for 7 seconds to call itself which will again change combi to the next one
     setTimeout(function(){
+
+        document.getElementById("instruction").innerHTML="";
+        user_input=document.getElementById("answer").value;
+
+        //test if what user has typed is equal to what was just shown
+        //Need to think about which input is finally taken as input
+        //For example, it has to be the correct password just once any time
+        //regardless of the final answer
+        //One way is to think of only taking the input when the enter key is pressed
+        //In other words, take what is in the input form when the enter key is pressed 
+
+        if(combination[i]==user_input){
+            score=score+1;
+        }
+        
+
+
+    
+        else{
+            score=score+0;
+        }
+
+        var list=document.getElementById("form");
+        list.removeChild(document.getElementById("answer"));
         show_disappear(--i);
-    },7000);
 
-
-
+    },10000);
+    
 
 }
 
-show_disappear(3);
+function start(){
+
+    show_disappear(9);
+
+}
+
+
+
+
+
+
 
 
